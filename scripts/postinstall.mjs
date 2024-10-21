1178a0dbf05994afed70b9dfbb5eab93f1565b9f
Pedro Estevão <contato@pedroestevao.com>
2024-10-21 19:17:16 -0300

    Update postinstall.mjs

--- scripts/postinstall.mjs
+++ scripts/postinstall.mjs
@@ -7,7 +7,7 @@ try {
         execSync('bun run prisma:migrate-dev', { stdio: 'inherit' });
     } else {
         execSync('bun run prisma:migrate-prod', { stdio: 'inherit' });
-        execSync('bunx prisma:generate', { stdio: 'inherit' });
+        execSync('bun run prisma:generate', { stdio: 'inherit' });
     }
 } catch (error) {
     console.error('Error running migrations:', error);
