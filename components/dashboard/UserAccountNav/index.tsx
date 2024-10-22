4d52aa11215272e348c70aa3aeff5c085a8054d1
Pedro-Estevao <contato@pedroestevao.com>
2024-10-22 17:17:38 -0300

    Update index.tsx

--- components/dashboard/UserAccountNav/index.tsx
+++ components/dashboard/UserAccountNav/index.tsx
@@ -18,18 +18,18 @@ const UserAccountNav = () => {
 
     const { isMobile } = useMediaQuery();
 
-    if (!user) {
-        return (
-            <div className="size-8 animate-pulse rounded-full border bg-muted" />
-        );
-    }
+    // if (!user) {
+    //     return (
+    //         <div className="size-8 animate-pulse rounded-full border bg-muted" />
+    //     );
+    // }
 
     if (isMobile) {
         return (
             <Drawer open={open} onClose={closeDrawer}>
                 <DrawerTrigger onClick={() => setOpen(true)}>
                     <UserAvatar
-                        user={{ name: user.name || null, image: user.image || null }}
+                        user={{ name: user?.name || null, image: user?.image || null }}
                         className="size-9 border"
                     />
                 </DrawerTrigger>
@@ -45,17 +45,17 @@ const UserAccountNav = () => {
 
                         <div className="flex items-center justify-start gap-2 p-2">
                             <div className="flex flex-col">
-                                {user.name && <p className="font-medium">{user.name}</p>}
-                                {user.email && (
+                                {user?.name && <p className="font-medium">{user.name}</p>}
+                                {user?.email && (
                                     <p className="w-[200px] truncate text-muted-foreground">
-                                        {user?.email}
+                                        {user.email}
                                     </p>
                                 )}
                             </div>
                         </div>
 
                         <ul role="list" className="mb-14 mt-1 w-full text-muted-foreground">
-                            {user.role === "ADMIN" ? (
+                            {user?.role === "ADMIN" ? (
                                 <li className="rounded-lg text-foreground hover:bg-muted">
                                     <Link
                                         href="/admin"
@@ -113,68 +113,75 @@ const UserAccountNav = () => {
     }
 
     return (
-        <DropdownMenu open={open} onOpenChange={setOpen}>
-            <DropdownMenuTrigger>
-                <UserAvatar
-                    user={{ name: user.name || null, image: user.image || null }}
-                    className="size-8 border"
-                />
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-                <div className="flex items-center justify-start gap-2 p-2">
-                    <div className="flex flex-col space-y-1 leading-none">
-                        {user.name && <p className="font-medium">{user.name}</p>}
-                        {user.email && (
-                            <p className="w-[200px] truncate text-sm text-muted-foreground">
-                                {user?.email}
-                            </p>
-                        )}
-                    </div>
-                </div>
-                <DropdownMenuSeparator />
+        <>
+            {!user ? (
+                <div className="size-8 animate-pulse rounded-full border bg-muted" />
+            ) : (
+                <DropdownMenu open={open} onOpenChange={setOpen}>
+                    <DropdownMenuTrigger>
+                        <UserAvatar
+                            user={{ name: user.name || null, image: user.image || null }}
+                            className="size-8 border"
+                        />
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <div className="flex items-center justify-start gap-2 p-2">
+                            <div className="flex flex-col space-y-1 leading-none">
+                                {user.name && <p className="font-medium">{user.name}</p>}
+                                {user.email && (
+                                    <p className="w-[200px] truncate text-sm text-muted-foreground">
+                                        {user?.email}
+                                    </p>
+                                )}
+                            </div>
+                        </div>
+                        <DropdownMenuSeparator />
 
-                {user.role === "ADMIN" ? (
-                    <DropdownMenuItem asChild>
-                        <Link href="/admin" className="flex items-center space-x-2.5">
-                            <Lock className="size-4" />
-                            <p className="text-sm">Admin</p>
-                        </Link>
-                    </DropdownMenuItem>
-                ) : null}
+                        {user.role === "ADMIN" ? (
+                            <DropdownMenuItem asChild>
+                                <Link href="/admin" className="flex items-center space-x-2.5">
+                                    <Lock className="size-4" />
+                                    <p className="text-sm">Admin</p>
+                                </Link>
+                            </DropdownMenuItem>
+                        ) : null}
 
-                <DropdownMenuItem asChild>
-                    <Link href="/dashboard" className="flex items-center space-x-2.5">
-                        <LayoutDashboard className="size-4" />
-                        <p className="text-sm">Dashboard</p>
-                    </Link>
-                </DropdownMenuItem>
+                        <DropdownMenuItem asChild>
+                            <Link href="/dashboard" className="flex items-center space-x-2.5">
+                                <LayoutDashboard className="size-4" />
+                                <p className="text-sm">Dashboard</p>
+                            </Link>
+                        </DropdownMenuItem>
 
-                <DropdownMenuItem asChild>
-                    <Link
-                        href="/dashboard/settings"
-                        className="flex items-center space-x-2.5"
-                    >
-                        <Settings className="size-4" />
-                        <p className="text-sm">Settings</p>
-                    </Link>
-                </DropdownMenuItem>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem
-                    className="cursor-pointer"
-                    onSelect={(event) => {
-                        event.preventDefault();
-                        signOut({
-                            callbackUrl: `${window.location.origin}/`,
-                        });
-                    }}
-                >
-                    <div className="flex items-center space-x-2.5">
-                        <LogOut className="size-4" />
-                        <p className="text-sm">Log out </p>
-                    </div>
-                </DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
+                        <DropdownMenuItem asChild>
+                            <Link
+                                href="/dashboard/settings"
+                                className="flex items-center space-x-2.5"
+                            >
+                                <Settings className="size-4" />
+                                <p className="text-sm">Settings</p>
+                            </Link>
+                        </DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem
+                            className="cursor-pointer"
+                            onSelect={(event) => {
+                                event.preventDefault();
+                                signOut({
+                                    callbackUrl: `${window.location.origin}/`,
+                                });
+                            }}
+                        >
+                            <div className="flex items-center space-x-2.5">
+                                <LogOut className="size-4" />
+                                <p className="text-sm">Log out </p>
+                            </div>
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+
+            )}
+        </>
     );
 };
 
