1f1a7e2f71b6b997752d566750c6f4723d3a0593
Pedro Estevão <contato@pedroestevao.com>
2024-10-22 19:46:16 -0300

    Create loading.tsx

---
+++ app/(protected)/dashboard/loading.tsx
@@ -0,0 +1,13 @@
+import DashboardHeader from "@/components/dashboard/Header";
+import { Skeleton } from "@/components/ui/skeleton";
+
+const DashboardLoading = () => {
+    return (
+        <>
+            <DashboardHeader heading="Dashboard" text="Current Role:" />
+            <Skeleton className="size-full rounded-lg" />
+        </>
+    );
+};
+
+export default DashboardLoading;
