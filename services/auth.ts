868266ce95863bace8e8ca9eb45c81e5f25d1593
Pedro-Estevao <contato@pedroestevao.com>
2024-10-22 14:40:18 -0300

    Update auth.ts

--- services/auth.ts
+++ services/auth.ts
@@ -17,6 +17,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         Google({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            allowDangerousEmailAccountLinking: true,
         }),
         Credentials({
             async authorize(credentials) {
